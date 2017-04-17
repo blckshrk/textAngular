d6088894940bfc48579198fa7e9244fc0fc4468c
Alexandre Bonhomme <alexand.bonhom@gmail.com>
2017-04-17 15:51:51 +0200

    Partial reverse of cd9de24cb109ec5fe96ac276a14802fd917e2125 to avoid shift+enter bug on Chrome

--- src/taBind.js
+++ src/taBind.js
@@ -903,28 +903,28 @@ angular.module('textAngular.taBind', ['textAngular.factories', 'textAngular.DOM'
                                             taSelection.setSelectionToElementStart(_new[0]);
                                         }
                                     } else {
-                                        // shift + Enter
-                                        var tagName = selection.tagName.toLowerCase();
-                                        //console.log('Shift+Enter', selection.tagName, attrs.taDefaultWrap, selection.innerHTML.trim());
-                                        // For an LI: We see: LI p ....<br><br>
-                                        // For a P: We see: P p ....<br><br>
-                                        // on Safari, the browser ignores the Shift+Enter and acts just as an Enter Key
-                                        // For an LI: We see: LI p <br>
-                                        // For a P: We see: P p <br>
-                                        if((tagName === attrs.taDefaultWrap ||
-                                            tagName === 'li' ||
-                                            tagName === 'pre' ||
-                                            tagName === 'div') &&
-                                            !/.+<br><br>/.test(selection.innerHTML.trim())) {
-                                            var ps = selection.previousSibling;
-                                            //console.log('wrong....', ps);
-                                            // we need to remove this selection and fix the previousSibling up...
-                                            if (ps) {
-                                                ps.innerHTML = ps.innerHTML + '<br><br>';
-                                                angular.element(selection).remove();
-                                                taSelection.setSelectionToElementEnd(ps);
-                                            }
-                                        }
+                                        // // shift + Enter
+                                        // var tagName = selection.tagName.toLowerCase();
+                                        // //console.log('Shift+Enter', selection.tagName, attrs.taDefaultWrap, selection.innerHTML.trim());
+                                        // // For an LI: We see: LI p ....<br><br>
+                                        // // For a P: We see: P p ....<br><br>
+                                        // // on Safari, the browser ignores the Shift+Enter and acts just as an Enter Key
+                                        // // For an LI: We see: LI p <br>
+                                        // // For a P: We see: P p <br>
+                                        // if((tagName === attrs.taDefaultWrap ||
+                                        //     tagName === 'li' ||
+                                        //     tagName === 'pre' ||
+                                        //     tagName === 'div') &&
+                                        //     !/.+<br><br>/.test(selection.innerHTML.trim())) {
+                                        //     var ps = selection.previousSibling;
+                                        //     //console.log('wrong....', ps);
+                                        //     // we need to remove this selection and fix the previousSibling up...
+                                        //     if (ps) {
+                                        //         ps.innerHTML = ps.innerHTML + '<br><br>';
+                                        //         angular.element(selection).remove();
+                                        //         taSelection.setSelectionToElementEnd(ps);
+                                        //     }
+                                        // }
                                     }
                                 }
                                 var val = _compileHtml();
